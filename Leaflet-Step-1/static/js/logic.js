992cbf15936b81d7142caaace94d43ee2835b1eb
zombimaki <andrewbperez@gmail.com>
2021-01-31 11:14:42 -0600

    datetime

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
-// All-Day Earthquake Map
+// Earthquake Activity Map 
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-// url of earthquake data
+// url of earthquake activity data
 var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
  // Create and center map
@@ -11,66 +11,43 @@ var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_da
   zoom: 1.5
 });
 
+//retrieve json data and execute createMap function
 d3.json(quakeURL, function(data) {
   quakeData = data.features;
   createMap(quakeData);
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-  //  plot the earthquake data from the quakeURL geojson data
-  ///////////////////////////////////////////////////////////////////////////////////////////
+//  create map function plots the earthquake and tetonic plate data
+///////////////////////////////////////////////////////////////////////////////////////////
 
 function createMap(quakeData) { 
     
-    // function returns the stlye for the markers
-    function getStyle(feature) {
+      ///////////////////////////////////////////////////////////////////////////////////////////
+      // map and map layers
+      ///////////////////////////////////////////////////////////////////////////////////////////
 
-      markerStyle = {
-        opacity: 0.75,
-        fillOpacity: 1,      
-        color: "black",
-        stroke: true,
-        weight: 0.75,
-        // set fillcolor by passing quake depth to getColor
-        fillColor: getColor(feature.geometry.coordinates[2]),
-        // set radius by passing quake magnitude to quakeRadius
-        radius: quakeRadius(feature.properties.mag),
-
-      };
-      return markerStyle
-    }
-    
-    // getColor function returns colors based on quake depth
-      function getColor(depth) {
-
-        var color = ((depth >= 100) ? ("#EE0000") : ((depth >= 80) ? ("#FF6333") : ((depth >= 60) ? ("#FFA500") : ((depth >= 40) ? ("#FFCC11"):  ((depth >= 20) ? ("#FFEE00"):"#ABCD00")))))
-      
-        return color;
-    }
-      
-
-      // set quake radius based on the quake magnitude
-      function quakeRadius(magnitude) {
-        radius = ((magnitude === 0) ? (1) : magnitude * 5)
-        return radius;
-    }
-
-      // create layer of quake locations
+      // create quake activity layer
       quakeLayer = L.geoJson(quakeData, {
+        pointToLayer: function(feature, location) {          
 
-        pointToLayer: function(feature, location) {
           return L.circleMarker(location);
         },
         style: getStyle,
-
         onEachFeature: function(feature, layer) {
-          layer.bindPopup("Place: " + feature.properties.place+ "<br>Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2]);
-        }
 
+          // format datetime to make more readable
+          var propertyTime = new Date(feature.properties.time);
+          var quakeTime = propertyTime.toUTCString();
+          // format first charater in string to be capitalized
+          var initCapPlace = feature.properties.place.charAt(0).toUpperCase() + feature.properties.place.slice(1);
+
+          layer.bindPopup("Place: " + initCapPlace +  "<br>Time: " + quakeTime + "<br>Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2]);
+        }
       }).addTo(myMap);
       
 
-      // set streetmap layer
+      // create streetmap layer
       streetLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
         attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
         tileSize: 512,
@@ -79,6 +56,40 @@ function createMap(quakeData) {
         id: "mapbox/streets-v11",
         accessToken: API_KEY
       }).addTo(myMap);
+
+      ///////////////////////////////////////////////////////////////////////////////////////////
+      // set color and size of markers 
+      ///////////////////////////////////////////////////////////////////////////////////////////
+
+      // returns the style for the markers
+      function getStyle(feature) {
+
+        markerStyle = {
+          opacity: 0.75,
+          fillOpacity: 1,      
+          color: "black",
+          stroke: true,
+          weight: 0.75,
+          // set fillcolor by passing quake depth to getColor function
+          fillColor: getColor(feature.geometry.coordinates[2]),
+          // set radius by passing quake magnitude to quakeRadius function
+          radius: quakeRadius(feature.properties.mag),
+
+        };
+        return markerStyle
+      }
+
+      // returns colors based on quake depth
+      function getColor(depth) {
+        var color = ((depth >= 100) ? ("#EE0000") : ((depth >= 80) ? ("#FF6333") : ((depth >= 60) ? ("#FFA500") : ((depth >= 40) ? ("#FFCC11"):  ((depth >= 20) ? ("#FFEE00"):"#ABCD00")))))
+        return color;
+      }      
+
+      // set quake radius based on the quake magnitude
+      function quakeRadius(magnitude) {
+        radius = ((magnitude === 0) ? (1) : magnitude * 5)
+        return radius;
+      }
 
       ///////////////////////////////////////////////////////////////////////////////////////////
       // create legend 
@@ -100,5 +111,4 @@ function createMap(quakeData) {
         };
 
         legend.addTo(myMap);
-
 }
--- Leaflet-Step-2/static/js/logic.js
+++ Leaflet-Step-2/static/js/logic.js
@@ -1,14 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////////////////
-// All-Day Earthquake Map
+// Earthquake Activity Map with Tetonic Plates
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-// url of earthquake and tetonic plate data
+// url of earthquake activity and tetonic plate data
 var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 var tetonicURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
-
-
-// use d3.json to read in the api data
+//retrieve json data and execute createMap function
 d3.json(quakeURL, function(data) {
   var quakeData = data.features;
   
@@ -17,69 +15,47 @@ d3.json(quakeURL, function(data) {
     var tetonicData = data.features;
 
     createMap(quakeData, tetonicData);
-
   });
 });
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////
-//  crerate map function plot the earthquake tetonic plate data
+//  create map function plots the earthquake and tetonic plate data
 ///////////////////////////////////////////////////////////////////////////////////////////
 
-function createMap(quakeData,tetonicData) {     
-
-    
-    // function returns the stlye for the markers
-  function getStyle(feature) {
-
-    markerStyle = {
-      opacity: 0.75,
-      fillOpacity: 1,      
-      color: "black",
-      stroke: true,
-      weight: 0.75,
-      // set fillcolor by passing quake depth to getColor
-      fillColor: getColor(feature.geometry.coordinates[2]),
-      // set radius by passing quake magnitude to quakeRadius
-      radius: quakeRadius(feature.properties.mag),
-
-    };
-    return markerStyle
-  }
-
-  // add comment
-  function makePolyline(feature, layer){
-    L.polyline(feature.geometry.coordinates);
-  }
-
+function createMap(quakeData,tetonicData) {       
   
   ///////////////////////////////////////////////////////////////////////////////////////////
-  // map layers
+  // map and map layers
   ///////////////////////////////////////////////////////////////////////////////////////////
-
   
-  // quake layer 
+  // create quake activity layer
   var quakeLayer = L.geoJson(quakeData, {
     pointToLayer: function(feature, location) {
       return L.circleMarker(location);
     },
     style: getStyle,
     onEachFeature: function(feature, layer) {
-      layer.bindPopup("Place: " + feature.properties.place+ "<br>Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2]);
+
+      var propertyTime = new Date(feature.properties.time);
+      var quakeTime = propertyTime.toUTCString();
+      // format first charater in string to be capitalized
+      var initCapPlace = feature.properties.place.charAt(0).toUpperCase() + feature.properties.place.slice(1);
+
+      layer.bindPopup("Place: " + initCapPlace + "<br>Time: " + quakeTime + "<br>Magnitude: " + feature.properties.mag + "<br>Depth: " + feature.geometry.coordinates[2]);
     }
 
   });
 
-  // tetonic plates layer
+  // create tetonic plates layer
   tetonicLayer = L.geoJSON(tetonicData, {
     onEachFeature: makePolyline,
       style: {
-        color: 'red',
+        color: 'grey',
         opacity: 0.9
       }
   });
 
-  // streetmap layer
+  // create streetmap layer
   var streetLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -88,21 +64,26 @@ function createMap(quakeData,tetonicData) {
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   });  
+
+  // generate the tetonic plate polyline
+  function makePolyline(feature, layer){
+    L.polyline(feature.geometry.coordinates);
+  }
   
   // create overlay maps
-
   var overlayMaps = {
     Earthquakes: quakeLayer,
     Plates : tetonicLayer
   };
 
-    // Create and center map
+  // Create and center map
   var myMap = L.map("map", {
     center: [19, -6],
     zoom: 1.5,
     layers: [quakeLayer,streetLayer]
   });
 
+  // create base map variable
   var baseMaps = {
     "Street Map": streetLayer,
     // "Dark Map": darkmap
@@ -111,8 +92,25 @@ function createMap(quakeData,tetonicData) {
   ///////////////////////////////////////////////////////////////////////////////////////////
   // set color and size of markers 
   ///////////////////////////////////////////////////////////////////////////////////////////
-  
 
+  // returns the style for the markers
+  function getStyle(feature) {
+
+    markerStyle = {
+      opacity: 0.75,
+      fillOpacity: 1,      
+      color: "black",
+      stroke: true,
+      weight: 0.75,
+      // set fillcolor by passing quake depth to getColor function
+      fillColor: getColor(feature.geometry.coordinates[2]),
+      // set radius by passing quake magnitude to quakeRadius function
+      radius: quakeRadius(feature.properties.mag),
+
+    };
+    return markerStyle
+  }
+  
   // set quake radius based on the quake magnitude
   function quakeRadius(magnitude) {
     radius = ((magnitude === 0) ? (1) : magnitude * 5)
@@ -121,9 +119,7 @@ function createMap(quakeData,tetonicData) {
 
   // getColor function returns colors based on quake depth
   function getColor(depth) {
-
     var color = ((depth >= 100) ? ("#EE0000") : ((depth >= 80) ? ("#FF6333") : ((depth >= 60) ? ("#FFA500") : ((depth >= 40) ? ("#FFCC11"):  ((depth >= 20) ? ("#FFEE00"):"#ABCD00")))))
-  
     return color;
 }
   ///////////////////////////////////////////////////////////////////////////////////////////
@@ -148,9 +144,9 @@ function createMap(quakeData,tetonicData) {
     legend.addTo(myMap);
 
 
-    ///////////////////////////////////////////////////////////////////////////////////////////
-    // add layer control 
-    ///////////////////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  // add layer control 
+  ///////////////////////////////////////////////////////////////////////////////////////////
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
